2780a750fdecf77900d76a42b3d2b6be5be3cfc4
Orest Dubay <orest3.dubay@gmail.com>
2017-03-21 21:34:27 +0100

    export to json files working

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ extern crate mongodb;
 
 use std::collections::BTreeMap;
 use clap::{Arg, App};
-use std::io::{BufReader, Read};
+use std::io::{BufReader, Read, Write};
 use std::fs::File;
 use std::str;
 use xml::reader::{EventReader, XmlEvent};
@@ -164,7 +164,8 @@ impl ToJson for Record {
     }
 }
 
-fn do_read(r: &mut Read) {
+fn do_read(r: &mut Read, json_output: Option<&str>) {
+    let mut number = 0;
     let file = BufReader::new(r);
     let parser = EventReader::new(file);
     let mut stack: Vec<Record> = Vec::new();
@@ -180,8 +181,15 @@ fn do_read(r: &mut Read) {
                 println!("End   {}", name);
                 if let Some(mut rec) = stack.pop() {
                     if stack.is_empty() {
+                        number += 1;
                         println!("--> {:?}", rec);
                         println!("JSON: {}", rec.to_json());
+                        if let Some(path) = json_output {
+                            File::create(format!("{}/{}.json", path, number))
+                                .and_then(
+                                    |mut f| f.write_all(format!("{}", rec.to_json()).as_bytes())
+                                ).expect("Error writing document")
+                        }
                     } else {
                         if let Some(mut parent) = stack.pop() {
                             parent.add_child(rec);
@@ -224,6 +232,30 @@ fn main() {
             .value_name("FMT")
             .help("Input format")
             .takes_value(true))
+        .arg(Arg::with_name("json")
+            .short("j")
+            .long("json")
+            .value_name("PATH")
+            .help("Export to json files")
+            .takes_value(true))
+        .arg(Arg::with_name("mongodb")
+            .short("m")
+            .long("mongodb")
+            .value_name("URI")
+            .help("Export to MongoDB, specify URI, e.g. mongodb://localhost:27017")
+            .takes_value(true))
+        .arg(Arg::with_name("db")
+            .short("d")
+            .long("db")
+            .value_name("DATABASE")
+            .help("MongoDB database")
+            .takes_value(true))
+        .arg(Arg::with_name("collection")
+            .short("c")
+            .long("collection")
+            .value_name("COLLECTION")
+            .help("MongoDB collection")
+            .takes_value(true))
         .get_matches();
 
     let input = matches.value_of("input").unwrap_or("ESStatistikListeModtag.xml");
@@ -233,14 +265,20 @@ fn main() {
     match format {
         "xml" => {
             println!("READ XML {}", input);
-            do_read(&mut File::open(input).unwrap_or_else(|err| panic!("{}\nCan't open file {}", err, input)))
+            do_read(
+                &mut File::open(input).unwrap_or_else(|err| panic!("{}\nCan't open file {}", err, input)),
+                matches.value_of("json")
+            )
         },
 
         "zip" => {
             println!("READ ZIP {}", input);
             let f = File::open(input).unwrap_or_else(|err| panic!("{}\nCan't open file {}", err, input));
             let mut archive = Box::new(zip::ZipArchive::new(f).unwrap_or_else(|err| panic!("{}\nCan't open zip archive {}", err, input)));
-            do_read(&mut archive.by_index(0).unwrap_or_else(|err| panic!("{}\nCan't open zipped file {}", err, input)))
+            do_read(
+                &mut archive.by_index(0).unwrap_or_else(|err| panic!("{}\nCan't open zipped file {}", err, input)),
+                matches.value_of("json")
+            )
         }
         _ => panic!("Unsupported format: {}", format)
     };
